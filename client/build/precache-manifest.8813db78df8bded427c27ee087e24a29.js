8b16dd8d3cd1ce6ab002ce421cd81333d90da4f2
Muhammad Saad ur Rehman <saadurrehman59@gmail.com>
2022-04-17 20:12:30 +0500

    refactor for deployment

---
+++ client/build/precache-manifest.8813db78df8bded427c27ee087e24a29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "993a8733f0e5c95af462f01775b0674a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf195ec47f859e0857f6",
+    "url": "/static/css/main.c2edbcf1.chunk.css"
+  },
+  {
+    "revision": "8fc623ce3faafd80267a",
+    "url": "/static/js/2.1f902f74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f902f74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf195ec47f859e0857f6",
+    "url": "/static/js/main.79451010.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.get("*", (req, resp) => {
+    resp.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, console.log("Started server on port:: ", PORT));
