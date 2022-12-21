e8a2b54c70fe8dd58395d4cc26bd10ad71516f05
Muhammad Saad ur Rehman <saadurrehman59@gmail.com>
2022-12-22 00:36:54 +0500

    new commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+console.log("app");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
   app.get("*", (req, resp) => {
